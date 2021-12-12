2ed2d1a29fd214e92518223a5f764bf9c50c2242
songhanpoo <33808968+songhanpoo@users.noreply.github.com>
2021-12-12 20:10:29 +0700

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:]) {
     // sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
-
+    echo "${config}"
   final pythonContent = libraryResource('src/test.py')
   // There are definitely better ways to do this without having to write to the consumer's workspace
   writeFile(file: 'test.py', text: pythonContent)
