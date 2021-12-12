df9f07f94ddd86c41739492dedb34468208b1f1b
hieunlp <hieunlp@techcombank.com.vn>
2021-12-12 19:57:36 +0700

    first commit

---
+++ src/test.py
@@ -0,0 +1,7 @@
+import sys
+
+def greeting(test:str):
+  print(test)
+
+if __name__ == "__main__":
+  greeting(test= sys.argv[1])
---
+++ vars/test.groovy
@@ -0,0 +1,8 @@
+def call(Map config = [:]) {
+    // sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
+
+  final pythonContent = libraryResource('src/test.py')
+  // There are definitely better ways to do this without having to write to the consumer's workspace
+  writeFile(file: 'test.py', text: pythonContent)
+  sh("chmod +x test.py && ./test.py ${config.name}")
+}
