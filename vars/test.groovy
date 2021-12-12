5b44811fc018007d75332016a99ff7224c134e95
songhanpoo <33808968+songhanpoo@users.noreply.github.com>
2021-12-12 20:15:34 +0700

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,7 +1,7 @@
 def call(name) {
     // sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
   echo "${name}"
-  final pythonContent = libraryResource('src/test.py')
+  final pythonContent = libraryResource('/var/jenkins_home/workspace/testtttt/src/test.py')
   // There are definitely better ways to do this without having to write to the consumer's workspace
   writeFile(file: 'test.py', text: pythonContent)
   sh("chmod +x test.py && ./test.py ${name}")
