7466291fcedba4f84effd0c6cad24bb332ef50fd
hieunlp <hieunlp@techcombank.com.vn>
2021-12-12 20:27:17 +0700

    first commit

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,7 +1,7 @@
 def call(name) {
     // sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
   echo "${name}"
-  final pythonContent = libraryResource('resources/com/hieuhienhoa/test.py')
+  final pythonContent = libraryResource('./test.py')
   // There are definitely better ways to do this without having to write to the consumer's workspace
   writeFile(file: 'test.py', text: pythonContent)
   sh("chmod +x test.py && ./test.py ${name}")
---
+++ vars/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+import sys
+
+def greeting(test:str):
+  print("Hello Hello"+test)
+
+if __name__ == "__main__":
+  greeting(test= sys.argv[1])
