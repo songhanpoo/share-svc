7c1ca3aa1c67e5576a56431e0914db509f8701a2
hieunlp <hieunlp@techcombank.com.vn>
2021-12-12 20:24:31 +0700

    first commit

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,7 +1,7 @@
 def call(name) {
     // sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
   echo "${name}"
-  final pythonContent = libraryResource('/var/jenkins_home/workspace/testtttt@libs/share-svc/src/test.py')
+  final pythonContent = libraryResource('resources/com/hieuhienhoa/test.py')
   // There are definitely better ways to do this without having to write to the consumer's workspace
   writeFile(file: 'test.py', text: pythonContent)
   sh("chmod +x test.py && ./test.py ${name}")
