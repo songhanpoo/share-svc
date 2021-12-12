82bde8d3d73e52909f5549bd0e6faab8b936eb18
hieunlp <hieunlp@techcombank.com.vn>
2021-12-12 20:27:53 +0700

    first commit

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,6 +1,7 @@
 def call(name) {
     // sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
   echo "${name}"
+  sh '$PWD'
   final pythonContent = libraryResource('./test.py')
   // There are definitely better ways to do this without having to write to the consumer's workspace
   writeFile(file: 'test.py', text: pythonContent)
