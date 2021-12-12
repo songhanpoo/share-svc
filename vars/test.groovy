59f9f0a543ddb09bae0f6744932fff12800be923
songhanpoo <33808968+songhanpoo@users.noreply.github.com>
2021-12-12 20:14:31 +0700

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,9 +1,9 @@
 def call(name) {
     // sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
   echo "${name}"
-//   final pythonContent = libraryResource('src/test.py')
-//   // There are definitely better ways to do this without having to write to the consumer's workspace
-//   writeFile(file: 'test.py', text: pythonContent)
-//   sh("chmod +x test.py && ./test.py ${config.name}")
-// 
+  final pythonContent = libraryResource('src/test.py')
+  // There are definitely better ways to do this without having to write to the consumer's workspace
+  writeFile(file: 'test.py', text: pythonContent)
+  sh("chmod +x test.py && ./test.py ${name}")
+
 }
